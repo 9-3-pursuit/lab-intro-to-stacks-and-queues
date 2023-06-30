1b78b094bfc3f2c981e28b23e128c0a7e199e342
Vandhana-Mohan <mohan.vandhana@gmail.com>
2023-06-29 20:10:19 -0400

    add node, stack, push method

--- solution.js
+++ solution.js
@@ -1,6 +1,35 @@
 const { nums, words } = require("./data/data.js");
 const { inspect } = require("util");
 
+class Node {
+  constructor(data, next) {
+    this.data = data;
+    this.next = next;
+  }
+}
+
+class Stack {
+  constructor(top = null) {
+    this.top = top;
+  }
+  
+  push(data) {
+    let newNode = new Node(data);
+    newNode.next = this.top;
+    this.top = newNode;
+    return this.top;
+  }
+}
+
+class Queue {
+  constructor(first, last, size, maxValue) {
+    this.first = first;
+    this.last = last;
+    this.size = size;
+    this.maxValue = maxValue;
+  }
+}
+
 module.exports = {
   Node,
   Queue,
