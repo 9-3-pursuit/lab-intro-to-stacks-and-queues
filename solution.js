7a9cbaa881e816de62bc8873fbaa8ff4d8a40f2b
amber-king <amberking@pursuit.org>
2023-06-30 11:46:31 -0400

    push() method added

--- solution.js
+++ solution.js
@@ -1,6 +1,7 @@
 const { nums, words } = require("./data/data.js");
 const { inspect } = require("util");
 
+// create a Node class w/ properties - data & next
 class Node {
   constructor(data) {
     this.data = data;
@@ -8,12 +9,23 @@ class Node {
   }
 }
 
+// create a stack with properties - top
 class Stack {
   constructor(top = null) {
     this.top = top;
   }
+
+  // ✓ Can create a new node 
+  // ✓ Can create a new stack
+  // ✓ Can add to stack using push method
+  push(data) {
+    const newItem = new Node(data);
+    newItem.next = this.top;
+    this.top = newItem;
+  }
 }
 
+// Create a queue with properties - first,last,size,max value
 class Queue {
   constructor() {
     this.first = null;
