251fc23878bff41183bca4d2bf9af37d5b085613
Diandre Miller <millerdiandre@gmail.com>
2023-07-06 01:32:58 -0400

    lab

--- solution.js
+++ solution.js
@@ -1,6 +1,183 @@
 const { nums, words } = require("./data/data.js");
 const { inspect } = require("util");
 
+class Node {
+  constructor(data) {
+    this.data = data
+    this.next = null
+  }
+}
+
+class Stack {
+  constructor() {
+    this.top = null
+    this.size = 0
+  }
+  push(data) {
+    const newNode = new Node(data)
+    newNode.next = this.top
+    this.top = newNode
+    this.size++
+  }
+
+  pop() {
+    if (!this.top) {
+      return null
+    }
+
+    const removedNode = this.top
+    this.top = removedNode.next
+    removedNode.next = null
+    this.size--
+
+    return removedNode
+  }
+
+  size() {
+    let count = 0
+    let current = this.top
+
+    while (current !== null) {
+      count++
+      current = current.next
+    }
+    return count
+  }
+
+  isEmpty() {
+    if (this.top === null) {
+      return true
+    }
+
+    return false
+  }
+
+  peek() {
+    if (this.top) {
+      return this.top
+    }
+    return null
+  }
+
+  findMin() {
+    if (this.top === null) {
+      return null
+    }
+
+    let current = this.top
+    let min = current.data
+
+    while (current !== null) {
+      if (current.data < min) {
+        min = current.data 
+      }
+      current = current.next
+    }
+
+    return min
+  }
+
+  
+}
+
+class Queue{
+
+  constructor() {
+    this.first = null
+    this.last = null
+    this.size = 0
+  }
+
+  enqueue(data) {
+    const newNode = new Node(data)
+
+   
+
+    if (!this.first) {
+      this.first = newNode
+      this.last = newNode
+    } else {
+      this.last.next = newNode
+      this.last = newNode
+    }
+    this.size++
+   
+  }
+
+  dequeue() {
+
+    if (!this.first) {
+      return null
+    }
+
+    const removedNode = this.first
+    this.first = removedNode.next
+    removedNode.next = null
+    this.size--
+
+    if (!this.first) {
+      this.last = null
+    }
+
+    return removedNode.data
+    
+  }
+
+  size() {
+    let count = 0;
+
+    let current = this.first
+
+    while (current !== null) {
+      count++
+      current = current.next
+    }
+
+    return count
+
+   
+  
+  }
+
+  isEmpty() {
+    if (this.first === null) {
+      return true
+    }
+
+    return false
+  }
+
+  peek() {
+    return this.first
+  }
+
+  getLast() {
+    return this.last
+  }
+
+  findMax() {
+    if (this.first === null) {
+      return null
+    }
+
+    let current = this.first
+    let max = current.data
+
+    while (current !== null) {
+      if (current.data > max) {
+        max = current.data 
+      }
+      current = current.next
+    }
+
+    return max
+  }
+    
+}
+
+
+
+
 module.exports = {
   Node,
   Queue,
