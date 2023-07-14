741094f1cecda5b66fb3119e1b5ce817c63bfdfd
Ari-Jackson <112004676+Ari-Jackson@users.noreply.github.com>
2023-07-13 23:11:50 -0400

    ✨ Creates Node

--- solution.js
+++ solution.js
@@ -1,6 +1,16 @@
 const { nums, words } = require("./data/data.js");
 const { inspect } = require("util");
 
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.next = null;
+  }
+  setNext(node) {
+    this.next = node;
+  }
+}
+
 module.exports = {
   Node,
   Queue,
